6478f6cbd3b2b665e9e735e214868ec6af8ec162
pacheko-ke <oluochp6@gmail.com>
2026-08-06 10:54:09 +0300

    add type to e

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -11,14 +11,14 @@ export default function Register() {
     phone: "",
   });
 
-  const handleChange = (e:MouseEvent) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const nextStep = () => setStep(step + 1);
   const prevStep = () => setStep(step - 1);
 
-  const handleSubmit = (e:MouseEvent) => {
+  const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     console.log("Final Data:", formData);
     alert("Registration complete!");
