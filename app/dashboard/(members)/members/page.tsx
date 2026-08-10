104087fce2e9eb60e5cc2236db287a2b9f0e8cb3
pacheko-ke <oluochp6@gmail.com>
2026-08-10 13:58:12 +0300

    adjust padding on view members

--- app/dashboard/(members)/members/page.tsx
+++ app/dashboard/(members)/members/page.tsx
@@ -77,8 +77,8 @@ export default function MembersPage() {
   );
 
   return (
-    <div className="p-6 mt-10 flex flex-col gap-4">
-      <Link href="/dashboard/add-member" className="text-white bg-orange-400 px-4 py-2 rounded-md self-end">Add Member</Link>
+    <div className="px-2 mt-10 flex flex-col gap-4 md:mx-20">
+      <Link href="/dashboard/add-members" className="text-white bg-orange-400 px-4 py-2 rounded-md self-end">Add Member</Link>
       <h1 className="text-xl font-semibold mb-4">Members</h1>
 
       <div className="flex flex-col sm:flex-row gap-3 mb-4">
